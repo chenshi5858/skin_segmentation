ec9f656346bd6020172e25a1a35bafa30a08d6fb
Fernanda Pérez <faperez1@miuandes.cl>
2025-04-06 20:11:56 -0400

    codigo base de la tarea (sin experimentos)

--- skin_segmentation.py
+++ skin_segmentation.py
@@ -46,12 +46,13 @@ class SimpleModel(nn.Module):
         self.fc1 = nn.Linear(input_size, 8)
         self.fc2 = nn.Linear(8, 16)
         self.fc3 = nn.Linear(16, n_classes)  
+        self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.sigmoid(self.fc1(x))
-        x = self.sigmoid(self.fc2(x))
-        x = self.fc3(x)  
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x) 
         return x
 
 device = "cpu"
--- tester.py
+++ tester.py
@@ -9,14 +9,14 @@ class SimpleModel(nn.Module):
         self.fc1 = nn.Linear(input_size, 8)
         self.fc2 = nn.Linear(8, 16)
         self.fc3 = nn.Linear(16, n_classes)  
+        self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = self.sigmoid(self.fc1(x))
-        x = self.sigmoid(self.fc2(x))
-        x = self.fc3(x)  
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x) 
         return x
-
 
 model = SimpleModel(3, 1)
 model.load_state_dict(torch.load("skin_segmentation.pth"))
